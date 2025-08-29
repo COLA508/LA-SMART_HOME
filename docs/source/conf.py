0ddf9b724cf1c7620dd588fba3cde3a3c8a593a1
COLA508 <1849377878@QQ.COM>
2025-08-29 16:05:33 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-#import sphinx_conestack_theme
-from pygments.lexers import Python3Lexer
-import time
-
 project = 'LA_Smart_Home-Installation tutorial'
-copyright = '2024,Bot'
+copyright = '2024, Bot'
 author = 'Bot'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = ["myst_parser"]
+
+from pygments.lexers import Python3Lexer
 pygments_lexers = {
     "python-repl": Python3Lexer(),
 }
@@ -30,19 +19,22 @@ source_suffix = {
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
- -- Options for HTML output -------------------------------------------------
- https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'conestack'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_theme = 'press'
 
+# 指定 logo 图片（放在 docs/_static/Logo2.png）
+html_logo = "_static/Logo2.png"
+
+# 启用静态文件目录（logo / css）
 html_static_path = ['_static']
-html_logo = '_static/Logo2.png'
 
+# 主题选项
 html_theme_options = {
-     'logo_only': True,
-     'display_version': False,
- }
+    'logo_only': True,      # 只显示 logo，不显示项目名称
+    'display_version': False,  # 不显示版本号
+}
+
+# 自定义 CSS（可选，用于调整 logo 大小）
+html_css_files = [
+    'css/custom.css',
+]
